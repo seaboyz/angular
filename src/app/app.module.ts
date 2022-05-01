dd542ee8fee46fe0b0c49acc642265cd601bb3b0
seaboyz <seaboyz@hotmail.com>
2022-04-30 19:56:40 -0500

    Add warning component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ServerComponent } from "./server/server.component";
 import { ServersComponent } from './servers/servers.component';
+import { WarningAlertComponent } from "./warning-alert/warning-alert.component";
 
 @NgModule({
   declarations: [
     AppComponent,
     ServerComponent,
-    ServersComponent
+    ServersComponent,
+    WarningAlertComponent
   ],
   imports: [
     BrowserModule,
