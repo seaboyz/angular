ef5ed6ac3b0ad7a8e0a6528f592f1628c5ca6e5f
seaboyz <seaboyz@hotmail.com>
2022-05-18 12:02:35 -0500

    Add success Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,13 +8,14 @@ import { ServerComponent } from "./server/server.component";
 import { ServersComponent } from './servers/servers.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatButtonModule } from '@angular/material/button'
+import { SuccessAlertComponent } from "./success-alert/success-alert.component";
 
 @NgModule({
   declarations: [
     AppComponent,
     ServerComponent,
     ServersComponent,
-
+    SuccessAlertComponent
   ],
   imports: [
     BrowserModule,
